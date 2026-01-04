43754ce7426b6a441d43c6ad2e1b134814b1f886
veersingh-mu <singhveer4943@gmail.com>
2026-01-04 15:20:15 +0530

    Simple Java console calculator with Scanner and switch

---
+++ Calculator.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        double num1 = sc.nextDouble();
+        System.out.print("Enter operator (+, -, *, /): ");
+        char operator = sc.next().charAt(0);
+        System.out.print("Enter second number: ");
+        double num2 = sc.nextDouble();
+        double result;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Error: Division by zero");
+                    sc.close();
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Invalid operator");
+                sc.close();
+                return;
+        }
+        System.out.println("Result: " + num1 + " " + operator + " " + num2 + " = " + result);
+        sc.close();
+    }
+}
+
